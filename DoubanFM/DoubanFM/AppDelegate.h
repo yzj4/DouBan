ca25494dc281b8e4f9d40d8044b01e577458d9fa
l k j y <yzj19932@sina.com>
2016-05-05 09:12:16 +0800

    可以吗

---
+++ DoubanFM/DoubanFM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoubanFM
+//
+//  Created by l k j y on 16/5/5.
+//  Copyright © 2016年 尹照俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DoubanFM/DoubanFM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DoubanFM
+//
+//  Created by l k j y on 16/5/5.
+//  Copyright © 2016年 尹照俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
